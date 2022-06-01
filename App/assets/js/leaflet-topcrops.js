6299d1bb3f7c3a4aa8ad4dbba796140e3b58c3e8
Neda Ahmadi Jeshvaghane <n01405605@humber.ca>
2022-06-01 18:22:53 -0400

    leaflet js updated with selected countries

--- App/assets/js/leaflet-topcrops.js
+++ App/assets/js/leaflet-topcrops.js
@@ -1,18 +1,53 @@
-
-// Create the map object with a center and zoom level.
-let map = L.map("recomMap").setView([40, 0], 2);
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// The base Map:
+let outDoor = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1,
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
 
-// // Loop through the cities array and create one marker for each city.
-// cities.forEach(function(city) {
-//     console.log(city)
-//     L.marker(city.location).addTo(map);
-// });
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+let baseMaps = {
+    Outdoor: outDoor,
+    Dark: dark,
+    Satellite: satellite,
+  };
+
+let map = L.map('recomMap', {
+    center: [40, 0],
+    zoom: 2,
+    layers: [outDoor],
+    fullscreenControl: true
+});
+
+let countriesData = new L.LayerGroup();
+
+let overlays = {
+  "20 Populated Countires": countriesData,
+};
+
+L.control.layers(baseMaps, overlays).addTo(map);
+
+countriesData="https://raw.githubusercontent.com/SohrabRezaei/Global-Crop-Yield-Analysis/main/App/assets/js/GeoJSON/top20.geojson"
+
+d3.json(countriesData).then(function(data) {
+    console.log(data);
+  L.geoJSON(data).addTo(map);
+});
+
