115cee0a1190bed2ac0c756414213458409d4eb9
Neda Ahmadi Jeshvaghane <n01405605@humber.ca>
2022-06-05 22:06:56 -0400

    updated app.py and static files

--- App/app.py
+++ App/app.py
@@ -1,8 +1,9 @@
 #################################################
 # Import Dependencies
 #################################################
+from asyncio.windows_utils import pipe
 import os
-from flask import (Flask, render_template, jsonify, request, redirect)
+from flask import (Flask, render_template, jsonify, request, redirect,url_for)
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -14,6 +15,8 @@ import pandas as pd
 import pickle
 import numpy as np
 from config import password
+from class_modifier import Country_modifier
+
 
 # Flask Setup
 app = Flask(__name__)
@@ -27,16 +30,18 @@ engine = create_engine(f"postgresql://sohrabrezaei:{password}@project.cqupc8fzro
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-
-Yield = Base.classes.Yield
-Temp = Base.classes.temp
-Rain = Base.classes.rainfall
-Joined = Base.classes.joined
+# Temp = Base.classes.temp
+# Rain = Base.classes.rainfall
+# Joined = Base.classes.joined
 
 session = Session(engine)
 
 # Machine Learning Model 
-# stds = pickle.load(open("Machine_learning/model_pickle", 'rb'))
+
+
+with open(r"model_pickle.pkl", "rb") as input_file:
+   model = pickle.load(input_file)
+
 
 #######################################################################
 
@@ -48,13 +53,13 @@ def home():
 
 # **************************Yield Prediction****************************
 @app.route("/prediction")
-def yield_prediction():
+def yield_page():
     return render_template("prediction.html")
 
-@app.route("/api/prediction/<rainfall>/<pest>/<temp>/<crop>/<country>")
+@app.route("/api/predict/<country>/<crop>/<rainfall>/<pest>/<temp>/")
 def yield_prediction(rainfall,pest,temp,crop, country):
     to_scale = pd.DataFrame({"Average Rainfall (mm/year)":[float(rainfall)],"Pesticides (Tonnes)":[float(pest)],"Average Temperature":[float(temp)]})
-    scale = stds.transform(to_scale)
+    scale = model.transform(to_scale)
     crop_index = int(crop)
     country_index = int(country)
     encoded_arr = np.zeros(27)
@@ -63,12 +68,6 @@ def yield_prediction(rainfall,pest,temp,crop, country):
     pred_arr = np.hstack((scale, encoded_arr))
     pred_y = model.predict(pred_arr)
     return { "predicted": pred_y}
-    # "Average Rainfall (mm/year)", "Pesticides (Tonnes)", "Average Temperature"
-    # Scale  .transform(<temp>/<rainfall>/<pest>)
-    # OnHotcode =>one table
-        # pd.df(columns name based on our ML to create a table)
-        # then use if statements for each of em
-    # rf.predict(final table)
 
     """
     'x0_Bolivia', 'x0_Brazil', 'x0_Burundi', 'x0_Cameroon',
@@ -88,10 +87,10 @@ def weather_analysis():
 
 # **************************Crop Recommendation****************************
 @app.route("/crop_recommendation")
-def crop_recommendation():
+def crop_page():
     return render_template("recommendation.html")
 
-@app.route("/api/crop_recommendation")
+@app.route("/api/crops")
 def crop_recommendation():
     query = """
     SELECT "Country",
